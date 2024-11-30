c684ef7936f18f37fedfaabf1537ac4a5df8a6d9
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-11-30 09:26:43 +0000

    set favicon and header bar image

--- src/components/header-bar.tsx
+++ src/components/header-bar.tsx
@@ -3,10 +3,12 @@
 import React from 'react'
 import { ThemeToggle } from './theme-toggle'
 import Bio from './bio'
+import Image from 'next/image'
 
 export default function HeaderBar() {
     return (
         <header className="py-6 flex justify-between items-start">
+            <Image className="border-2 border-white rounded-md" src="/icon-32x32.png" alt="ens" width="32" height="32" />
             <ThemeToggle />
         </header>
     )
